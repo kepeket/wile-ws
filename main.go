93755619af324092d250a3e5f900365ed1e34ec6
Kevin Prettre <kevin.prettre@qonto.eu>
2020-11-23 11:29:40 +0100

    Root handler for clever cloud

--- main.go
+++ main.go
@@ -13,10 +13,20 @@ import (
 func main() {
 	log.SetOutput(os.Stderr)
 	router := mux.NewRouter()
+	router.HandleFunc("/", rootHandler)
 	router.HandleFunc("/chaussette", handler.WSHandler)
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
 
 	go handler.DispatchRoomMessage()
 	go handler.DispatchPingMessage()
 
-	fmt.Println(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+	fmt.Println(http.ListenAndServe(":"+port, router))
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
 }
